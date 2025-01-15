b145b3d59619eddbe667b44f6149b079c5fa822e
alsry922 <alsry922@gmail.com>
2025-01-15 17:37:36 +0900

    [302]

---
+++ src/app/users/users.routes.ts
@@ -0,0 +1,33 @@
+import {Routes} from '@angular/router';
+import {canLeaveEditPage, NewTaskComponent} from '../tasks/new-task/new-task.component';
+import {resolveUserTasks, TasksComponent} from "../tasks/tasks.component";
+import {TasksService} from "../tasks/tasks.service";
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    providers: [TasksService],
+    children:[
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        // loadComponent: () => import('../tasks/tasks.component').then(m => m.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
+      },
+    ]
+  }
+];
